e3a960cd3a5b8adcbababe3e9301efde8fc6e406
Сергей Герклоц <Gerklots.S.S@yandex.ru>
2020-04-16 22:05:07 +0300

    some fixes

--- vigenere cipher/Program.cs
+++ vigenere cipher/Program.cs
@@ -10,8 +10,9 @@ namespace vigenere_cipher
     {
         static void Main(string[] args)
         {
-            string a = "абвгдежзийклмнопрстуфхцчшщъыбэюя";
-            for (int i = 0; i < 32; i++)
+            Console.WriteLine("Матрица Виженера");
+            string a = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+            for (int i = 0; i < 33; i++)
             {
                 if (i != 0)
                     Console.Write("{0}\t| ", i);
@@ -25,38 +26,42 @@ namespace vigenere_cipher
                 Console.WriteLine();
             }
 
-            while(true)
-            {
+            Console.Write("Для продолжения нажмите любую клавишу...");
+
+            while (true)
+            { 
+                Console.ReadKey();
                 Console.Clear();
+
                 Console.WriteLine("\nШифр Виженера для русского алфавита\n");
                 Console.WriteLine("Чтобы зашифровать сообщение, нажмите 1.\nЧтобы дешифровать сообщение, нажмите 2.\nДля выхода, нажмите 0");
-                switch (Console.ReadLine())
+                switch (Console.ReadKey().KeyChar)
                 {
-                    case "0":
+                    case '0':
                         System.Environment.Exit(0);
                         break;
-                    case "1":
+                    case '1':
                         if (encrypt() == false)
                         {
                             Console.WriteLine("\nОшибка: были введены символы, отличные от русского алфавита.\n\nНажмите любую клавишу");
-                            Console.ReadKey();
+                            Console.ReadKey(true);
                         }
                         else
                         {
                             Console.WriteLine("\nШифрование завершено.\n\nНажмите любую клавишу");
-                            Console.ReadKey();
+                            Console.ReadKey(true);
                         }
                         break;
-                    case "2":
+                    case '2':
                         if (decrypt() == false)
                         {
-                            Console.WriteLine("\nОшибка: были введены символы, отличные от русского алфавита.\n\nНажмите любую клавишу");
-                            Console.ReadKey();
+                            Console.WriteLine("\nОшибка: были введены символы, отличные от русского алфавита.\n\nНажмите любую клавишу...");
+                            Console.ReadKey(true);
                         }
                         else
                         {
-                            Console.WriteLine("\nДешифрование завершено.\n\nНажмите любую клавишу");
-                            Console.ReadKey();
+                            Console.WriteLine("\nДешифрование завершено.\n\nНажмите любую клавишу...");
+                            Console.ReadKey(true);
                         }
                         break;
                     default:
@@ -69,25 +74,26 @@ namespace vigenere_cipher
         }
           static bool encrypt()
             {
-                 Console.Write("\nВведите сообщение: ");
+                Console.Clear();
+                Console.Write("\nВведите сообщение: ");
                 byte[] text = Encoding.GetEncoding(1251).GetBytes(Console.ReadLine());
 
                 Console.Write("\nВведите ключ: ");
                 byte[] key = Encoding.GetEncoding(1251).GetBytes(Console.ReadLine());
 
-
                 byte[] shifr = new byte[text.Length];
 
-
-                for (int i = 0; i < text.Length; i++)
-                {
+                for(int i = 0; i < text.Length; i++)
                     if ((text[i] < 224) || (key[i] < 224))
                         return false;
+                matrix(System.Text.Encoding.GetEncoding(1251).GetString((key)));
 
+            for (int i = 0; i < text.Length; i++)
+                {
                     text[i] = (byte)(text[i] - 224);
                     key[i] = (byte)(key[i] - 224);
                 }
-            Console.ReadKey();
+
                 for (int i = 0; i < text.Length; i++)
                 {
                     if (text[i] + key[i] >= 32)
@@ -97,19 +103,21 @@ namespace vigenere_cipher
 
                 }
             
-                Console.WriteLine(System.Text.Encoding.GetEncoding(1251).GetString((shifr)));
+                Console.WriteLine("\nШифрованное сообщение: {0}", System.Text.Encoding.GetEncoding(1251).GetString((shifr)));
             return true;
             }
         static bool decrypt()
         {
-            Console.Write("\nВведите ключ: ");
+            Console.Clear();
+            Console.Write("\nВведите шифр: ");
             byte[] key = Encoding.GetEncoding(1251).GetBytes(Console.ReadLine());
 
-            Console.Write("\nВведите зашифрованное сообщение: ");
+            Console.Write("\nВведите ключ: ");
             byte[] text = Encoding.GetEncoding(1251).GetBytes(Console.ReadLine());
 
             byte[] shifr = new byte[text.Length];
 
+            matrix(System.Text.Encoding.GetEncoding(1251).GetString((key)));
 
             for (int i = 0; i < text.Length; i++)
             {
@@ -129,8 +137,40 @@ namespace vigenere_cipher
 
             }
 
-            Console.WriteLine(System.Text.Encoding.GetEncoding(1251).GetString((shifr)));
+            Console.WriteLine("\nИсходное сообщение: {0}", System.Text.Encoding.GetEncoding(1251).GetString((shifr)));
             return true;
+        }
+
+        static void matrix(string input)
+        {
+            //Console.WriteLine((int)('а'));
+            string alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+            Console.Write("\nМатрица шифрования\n\t| ");
+            for (int i = 0; i < 32; i++)
+                Console.Write("{0} ", alphabet[i]);
+            Console.WriteLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                    if (alphabet[j] == input[i])
+                    {
+                        Console.Write("{0}\t| ", j);
+                        for (int k = 0; k < 32; k++)
+                        {
+                            if (1072 + j + k > 1103)
+                                Console.Write("{0} ", (char)(1072 + j + k - 32));
+                                //Console.Write("{0} ", (int)(1072 + j + k - 32));
+                            else
+                                Console.Write("{0} ", (char)(1072 + j + k));
+                                //Console.Write("{0} ", 1072 + j + k);
+                        }
+                        alphabet.Remove(j, 1);
+                        Console.WriteLine();
+
+                    }
+            }
+            
         }
     }
 }
